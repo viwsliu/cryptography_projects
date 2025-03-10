740ef8c7535c0b2647b14269e21bff598c92f9ad
viwsliu <vtliului@gmail.com>
2025-03-09 22:29:15 -0700

    use v3

--- AES_example/AES_runner.py
+++ AES_example/AES_runner.py
@@ -1,21 +1,37 @@
 from AES_test_v1 import AESEncryption
 from AES_test_v2 import AESCipher
+from AES_test_v3 import AESEncryption
+import os
+import base64
 
-key = 'mysecretkey'
+key = base64.b64encode(os.urandom(16)).decode('utf-8')
+
 cipher = AESEncryption(key)
 cipher2 = AESCipher(key)
+cipher3 = AESEncryption(key)
+
 # input = input("Input a message to encrypt: ")
-input = "tes"
+input = "test"
 
-encrypted1 = cipher.encrypt(input)
-print(f'Encrypted v1: {encrypted1}')
+def test_v1(input):
+    encrypted = cipher.encrypt(input)
+    print(f'Encrypted v1: {encrypted}')
+    decrypted = cipher.decrypt(encrypted)
+    print(f'Decrypted v1: {decrypted}')
 
-decrypted1 = cipher.decrypt(encrypted1)
-print(f'Decrypted v1: {decrypted1}')
+def test_v2(input):
+    encrypted = cipher2.encrypt(input)
+    print(f'Encrypted v2: {encrypted}')
+    decrypted = cipher2.decrypt(encrypted)
+    print(f'Decrypted v2: {decrypted}')
 
+def test_v3(input):
+    encrypted = cipher3.encrypt(input)
+    print(f'Encrypted v3: {encrypted}')
+    decrypted = cipher3.decrypt(encrypted)
+    print(f'Decrypted v3: {decrypted}')
 
-encrypted2 = cipher2.encrypt(input)
-print(f'Encrypted v2: {encrypted2}')
-
-decrypted2 = cipher2.decrypt(encrypted2)
-print(f'Decrypted v2: {decrypted2}')
+print("key: ", key)
+test_v1(input)
+test_v2(input)
+test_v3(input)
--- AES_example/AES_test_v1.py
+++ AES_example/AES_test_v1.py
@@ -2,46 +2,49 @@
 # I decided to use the "cryptography" python library, which exposes cryptographic recipes and primitives
 # https://cryptography.io/en/latest/
 
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-#for documentation on each, in order:
+from cryptography.hazmat.primitives.ciphers import Cipher, modes, algorithms
 # https://cryptography.io/en/latest/hazmat/primitives/symmetric-encryption/#cryptography.hazmat.primitives.ciphers.Cipher
 # https://cryptography.io/en/latest/hazmat/primitives/symmetric-encryption/#algorithms
 # https://cryptography.io/en/latest/hazmat/primitives/symmetric-encryption/#module-cryptography.hazmat.primitives.ciphers.modes
 
 from cryptography.hazmat.primitives import hashes
-#for documentation on imported:
 # https://cryptography.io/en/latest/hazmat/primitives/cryptographic-hashes/#cryptography.hazmat.primitives.hashes.Hash
 
 from cryptography.hazmat.backends import default_backend
-# Provides access to the underlying implementation of algorithms and operations such as encryption, decryption, and hashing 
+#provides access to the underlying implementation of algorithms and operations such as encryption, decryption, and hashing 
 
 import base64 # used to encode/decode binary data to/from ASCII text
-import os #used to generate random data for the Init Vector
-
+import os
+import s_box
 
 class AESEncryption(object):
+   
     def __init__(self, key):
         self.key = self._hash_key(key)
         self.iv = os.urandom(16)  # random 16-byte IV
-    
-    def _hash_key(self, key):
+        self.turns = 10 #since we will use a 128 byte key, we will perform 10 rounds of encryption
+        self.s_box, self.inv_s_box = s_box.generate_sbox()
+
+    def _hash_key(self, key): #takes a 128 bit key and produces a hashed output using SHA-256
         digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
-        digest.update(key.encode('utf-8'))
-        return digest.finalize()[:16]
+        digest.update(key.encode('utf-8')) #converts input key to bytes using UTF-8 endcoding, since hash functions work on byte data
+        return digest.finalize()[:16] #only return the first 16 bytes (128 bits of the hash)
 
-    def _pad(self, data):
-        block_size = 16
-        pad_len = block_size - len(data) % block_size
+    def _pad(self, data): #implements PKS-57 padding. used when plaintext is not a multiple of 16 bytes
+        block_size = 16 #standard block size of AES
+        pad_len = block_size - len(data) % block_size #calculate how many bytes are needed to reach the next multiple of 16
         padding = bytes([pad_len]) * pad_len
-        return data + padding
+        return data + padding 
 
-    def _unpad(self, data):
+    def _unpad(self, data): #removes padding from ciphertext when decrypting
         pad_len = data[-1]
         return data[:-pad_len]
 
     def encrypt(self, plaintext):
-        padded_plaintext = self._pad(plaintext.encode('utf-8'))
+        padded_plaintext = self._pad(plaintext.encode('utf-8')) #apply padding
         cipher = Cipher(algorithms.AES(self.key), modes.CBC(self.iv), backend=default_backend())
+        #AES enryption utilizes Cipher block chaining mode using the IV
+
         encryptor = cipher.encryptor()
         ciphertext = encryptor.update(padded_plaintext) + encryptor.finalize()
         return base64.b64encode(self.iv + ciphertext) #prepend the IV to the ciphertext and encode to base64
@@ -57,3 +60,5 @@ class AESEncryption(object):
         decrypted_padded = decryptor.update(actual_ciphertext) + decryptor.finalize()
         plaintext = self._unpad(decrypted_padded)
         return plaintext.decode('utf-8')
+    
+    # ---------------------------------------
