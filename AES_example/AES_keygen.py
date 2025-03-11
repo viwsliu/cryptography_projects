a3bcd0b9827c199c4d578b6d4006db401b728eb1
Vince L <vtliului@gmail.com>
2025-03-10 19:15:49 -0700

    sdfafsd

--- AES_example/AES_keygen.py
+++ AES_example/AES_keygen.py
@@ -1,19 +1,15 @@
-def generate_aes_key():
-  """Generates a random 128-bit AES key (16 bytes)."""
-  key = bytearray(16)
-  for i in range(16):
-    key[i] = _get_random_byte()
-  return bytes(key)
+import os
 
-def _get_random_byte():
-  """Generates a pseudo-random byte using system time."""
-  return (int(str(time.time() % 1)[-1]) + ord(str(time.time() % 1)[-2])) % 256
+def generate_aes_key():
+    """Generates a random 128-bit AES key (16 bytes) using secure randomness."""
+    return os.urandom(16)  # Secure and efficient
 
 if __name__ == '__main__':
-    import time
+    key_file = 'key.txt'
+
     aes_key = generate_aes_key()
     print(f"Generated AES-128 key: {aes_key.hex()}")
 
-    key_file = 'key.txt'
+    # Write the key to key.txt
     with open(key_file, 'w') as file:
         file.write(aes_key.hex())
